54cc0783c92b6acf48724218b43a80d4f329b96a
Almir Santos <almir.santos@umanni.com.br>
2018-10-13 16:19:55 -0300

    data and arguments

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,1 +1,69 @@
-console.log('hello graphql')
+import {
+  GraphQLServer
+} from 'graphql-yoga'
+
+//Type definition (schema)
+const typeDefs = `
+  type Query {
+    greeting(name: String, position: String): String!
+    id: ID!
+    name: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+    me: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+`
+
+//Resolvers
+
+const resolvers = {
+  Query: {
+    greeting(parent, args, ctx, info) {
+      if (args.name && args.position){
+        return `Hello ${args.name} you are my favorite ${args.position}`
+      }
+      return 'Hello'
+      
+    },
+    id() {
+      return 'abc123'
+    },
+    name() {
+      return 'Almir'
+    },
+    age() {
+      return '23'
+    },
+    employed() {
+      return true
+    },
+    gpa() {
+      return null
+    },
+    me() {
+      return {
+        id: '123',
+        name: 'Mike',
+        email: 'mike@email.com',
+        age: '33'
+      }
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(()=> {
+  console.log('server up!')
+})
